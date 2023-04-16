a4b462303ed570ae89efb82de8009cc12052a096
Joseph Ricafort <josephricafortjr@gmail.com>
2023-04-17 00:04:05 +0800

    Resfreshing bar chart

--- src/lib/BarChartLayer.svelte
+++ src/lib/BarChartLayer.svelte
@@ -1,126 +1,33 @@
 <script>
-  // @ts-ignore
-  import { LayerCake, ScaledSvg, Html } from 'layercake';
-  // @ts-ignore
-  import { scaleBand, scaleLinear } from 'd3-scale';
-  import { max } from 'd3-array';
+  import { LayerCake, Svg } from 'layercake';
+  import { scaleBand } from 'd3-scale';
 
-  // @ts-ignore
+  // import Bar from './lib/Bar.svelte';
   import Bar from './Bar.svelte';
   import AxisX from './AxisX.svelte';
   import AxisY from './AxisY.svelte';
-  import BasicChart from "./BasicChart.svelte"; 
-  //import data from "../../data/fiveyears_tally.json";
 
+  // This example loads csv data as json using @rollup/plugin-dsv
+  // import data from './_data/groups.csv';
 
-  const data= [
-    {
-      "main_party":"ERC",
-      "gained": 64,
-      "lost": 22,
-      "no_change": 567,
-      "total": 631
-    },
-    {
-      "main_party":"PSC",
-      "gained": 22,
-      "lost": 40,
-      "no_change": 384,
-      "total": 406
-    },
-    {
-      "main_party":"Ciutadans",
-      "gained": 0,
-      "lost": 4,
-      "no_change": 79,
-      "total": 79
-    },
-    {
-      "main_party":"Others",
-      "gained": 21,
-      "lost": 35,
-      "no_change": 212,
-      "total": 233
-    },
-    {
-      "main_party":"PP",
-      "gained": 3,
-      "lost": 0,
-      "no_change": 222,
-      "total": 225
-    },
-    {
-      "main_party":"JxC",
-      "gained": 2,
-      "lost": 0,
-      "no_change": 12,
-      "total": 14
-    },
-    {
-      "main_party":"CIU",
-      "gained": 3,
-      "lost": 5,
-      "no_change": 15,
-      "total": 18
-    },
-    {
-      "main_party":"Podem",
-      "gained": 1,
-      "lost": 9,
-      "no_change": 45,
-      "total": 46
-    },
-    {
-      "main_party":"CUP",
-      "gained": 0,
-      "lost": 0,
-      "no_change": 5,
-      "total": 5
-    },
-    {
-      "main_party":"PACMA",
-      "gained": 0,
-      "lost": 0,
-      "no_change": 3,
-      "total": 3
-    },
-  ]
+  const data = [
+    {x: 0, y: 0},
+    {x: 5, y: 10},
+    {x: 10, y: 20},
+    {x: 15, y: 30},
+    {x: 20, y: 40}
+  ];
 
-  const xKey = 'main_party';
-  const yKey = 'total';
-  
-  </script>
-  
-  <div class="chart-container">
-    <LayerCake
-      ssr={true}
-      percentRange={true}
-      padding={{ top: 0, right: 20, bottom: 20, left: 35 }}
-      x='main_party'
-      y='total'
-      xScale={scaleBand().paddingInner([0.05]).round(true)}
-      yScale={scaleLinear().nice()}
-      yDomain={[0, max(data, d => d.total)]}
-      xDomain={data.map(d=>d.main_party)}
-      data={data}
-    >
-      <Html>
-        <AxisX
-          gridlines={false}
-        />
-        <AxisY 
-        baseline={true}
-        gridlines={true}/>
-      </Html>
-      <ScaledSvg>
-        <BasicChart/>
-      </ScaledSvg>
-    </LayerCake>
-  
-  </div>
+  const xKey = 'value';
+  const yKey = 'year';
 
-  <style>
-    /*
+  data.forEach(d => {
+    d[xKey] = +d[xKey];
+  });
+</script>
+
+<style>
+  /*
     The wrapper div needs to have an explicit width and height in CSS.
     It can also be a flexbox child or CSS grid element.
     The point being it needs dimensions since the <LayerCake> element will
@@ -128,6 +35,29 @@
   */
   .chart-container {
     width: 100%;
-    height: 300px;
+    height: 250px;
   }
 </style>
+
+<div class="chart-container">
+  <LayerCake
+    padding={{ top: 0, bottom: 20, left: 35 }}
+    x={xKey}
+    y={yKey}
+    yScale={scaleBand().paddingInner([0.05])}
+    xDomain={[0, null]}
+    data={data}
+  >
+    <Svg>
+      <AxisX
+        gridlines={true}
+        baseline={true}
+        snapTicks={true}
+      />
+      <AxisY
+        gridlines={false}
+      />
+      <Bar/>
+    </Svg>
+  </LayerCake>
+</div>
