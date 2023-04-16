543356bb2fc70380d5aee5e959a62cebaa85ae35
Pere <peroc79@gmail.com>
2023-04-16 12:22:30 +0200

    Layercake config and first test

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,43 @@
 <script>
+   
 
+   import { LayerCake, ScaledSvg, Html } from 'layercake';
+  import { scaleBand } from 'd3-scale';
+
+  import Bar from './lib/Bar.svelte';
+  import AxisX from './lib/AxisX.svelte';
+  import AxisY from './lib/AxisY.svelte';
+   import BasicChart from "./lib/BasicChart.svelte"; 
+  import Tooltip from "./lib/Tooltip.svelte";
+
+  const xKey = 'value';
+  const yKey = 'year';
+
+  const data=[
+    {
+      "year": "2015",
+      "value": 2
+    },
+    {
+      "year": "2016",
+      "value": 4
+    },
+    {
+      "year": "2017",
+      "value": 7
+    },
+    {
+      "year": "2018",
+      "value": 1
+    },
+    {
+      "year": "2019",
+      "value": 1
+    }
+  ]
+
+  
+ 
 </script>
 
 <main>
@@ -8,6 +46,33 @@
     <p>By Marina Rovira Boix, Joseph Ricafort, Pere Roca Ristol</p>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   </header>
+  <div class="chart-container">
+    <LayerCake
+      ssr={true}
+      percentRange={true}
+      padding={{ top: 0, right: 20, bottom: 20, left: 35 }}
+      x='value'
+      y='year'
+      yScale={scaleBand().paddingInner([0.05]).round(true)}
+      yDomain={data.map(d=>d.year)}
+      xDomain={[0, null]}
+      data={data}
+    >
+      <Html>
+        <AxisX
+          gridlines={true}
+          baseline={true}
+          snapTicks={true}
+        />
+        <AxisY gridlines={false}/>
+      </Html>
+      <ScaledSvg>
+        <BasicChart/>
+      </ScaledSvg>
+    </LayerCake>
+  
+  </div>
+  <!-- <Counter/> -->
   <section class="introduction">
     <h2>Introduction</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
@@ -35,4 +100,8 @@ section, header {
   max-width: 700px;
   margin: 0 auto;
 }
+.chart-container {
+    width: 100%;
+    height: 250px;
+  }
 </style>
---
+++ src/lib/BasicChart.svelte
@@ -0,0 +1,40 @@
+<script>
+  import { LayerCake } from 'layercake';
+  import { getContext } from 'svelte';
+  const { data, xGet, yGet, xScale, yScale }= getContext("LayerCake");
+  // Define some data
+  const points = [
+    {x: 0, y: 0},
+    {x: 5, y: 10},
+    {x: 10, y: 20},
+    {x: 15, y: 30},
+    {x: 20, y: 40}
+  ];
+  export let fill = '#00bbff';
+</script>
+<g class="bar-group">
+  {#each $data as d, i}
+    <rect
+      class='group-rect'
+      data-id="{i}"
+      x="{$xScale.range()[0]}"
+      y="{$yGet(d)}"
+      height={$yScale.bandwidth()}
+      width="{$xGet(d)}"
+      {fill}
+    ></rect>
+  {/each}
+</g>
+<style>
+  /*
+    The wrapper div needs to have an explicit width and height in CSS.
+    It can also be a flexbox child or CSS grid element.
+    The point being it needs dimensions since the <LayerCake> element will
+    expand to fill it.
+  */
+  .chart-container {
+    width: 100%;
+    height: 300px;
+  }
+</style>
+
